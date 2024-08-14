a30ba091e68240f02a5a58898a7510889d07e6d5
allysson76 <allysson.pereira@iesb.edu.br>
2024-08-14 09:24:03 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-# for n in range(2, 10):
-    # for x in range(2, n):
-        # if n % x == 0:
-            # print(n, 'equals', x, '*', n//x)
-# 
-        # else:
-            # loop fell through without finding a factor
-            # print(n, 'is a prime number')
-        # 
-        # continue
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
 
-def my_function():
-    pass
+        else:
+            #loop fell through without finding a factor
+            print(n, 'is a prime number')
+        
+        continue
